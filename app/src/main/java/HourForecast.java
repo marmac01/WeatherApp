afaf60c1501ba9562b76a5bc3001b873e54c1001
marmac01 <michaelarmackay@gmail.com>
2022-09-29 14:14:02 +0100

    added Hour Forecast class

---
+++ app/src/main/java/HourForecast.java
@@ -0,0 +1,11 @@
+public class HourForecast {
+    private double temperature;
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+}
